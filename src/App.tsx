a48c6aafea6e0dbb33565ef47a040d739ea7bb58
dushyant kumar <dushyantvelar@gmail.com>
2025-05-06 12:04:15 +0530

    component setup with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import React,{ useState } from 'react'
+import React from 'react'
+import Index1 from './components/Todo/index1';
 
-import MyButton from './components/button';
-function App() {
+
+const myTodoItems=[
+  {
+    id:1,
+    title:"i have to finish my homework",
+  },
+  {
+    id:2,
+    title:"complete the lecture"
+  },
+  {
+    id:3,
+    title:"i have to complete the project "
+  }
+]
+const App:React.FC = () => {
   return (
-    <>
-      <MyButton onClick={()=>alert("okay")} text="seach"/>
-    </>
+    <div>
+      <Index1 items={myTodoItems}/>
+    </div>
   )
 }
-export default App;
+
+export default App
+
+
